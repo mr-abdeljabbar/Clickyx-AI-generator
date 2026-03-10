16abade75e3f0a750e6c08847d3af0772b3c66dd
mr-abdeljabbar <mr.abdeljabbarbougrine@gmail.com>
2026-03-10 18:25:45 +0000

    Optimize Prisma for Netlify: broad binary targets and logging

--- server/prisma.ts
+++ server/prisma.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+});
+
+console.log('[Prisma] Client instance created.');
 
 export default prisma;
